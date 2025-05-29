859594eb449a876fdacc8e860710f6d170b90746
Harry Ross <hazross@hotmail.com>
2025-05-29 13:47:13 +1000

    added telescope symbols

--- .config/nvim/lua/harry/setup.lua
+++ .config/nvim/lua/harry/setup.lua
@@ -9,6 +9,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, { desc = "Telescope document symbols" })
+vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, { desc = "Telescope workplace symbols" })
+
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -30,4 +33,3 @@ vim.keymap.set('n', '<leader>i', function()
   vim.lsp.buf.implementation()
 end, { desc = "Go to implementation" })
 
-vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Telescope document symbols" })
