60748c873095fbacff5f99692673a6c9a858cbe9
Harry Ross <hazross@hotmail.com>
2025-08-10 15:38:57 +1000

    added formatting

--- .config/nvim/lua/harry/plugins/treesitter.lua
+++ .config/nvim/lua/harry/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "astro", "css", "cpp", "javascript", "terraform", "typescript", "tsx" }
-  }
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "astro", "css", "cpp", "javascript", "terraform", "typescript", "tsx" },
+		auto_install = true,
+		highlight = { enable = true },
+	},
 }
-
