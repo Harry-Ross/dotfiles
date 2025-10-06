83f982bc31bb772e56822a26ebd551db66df4a5f
Harry Ross <hazross@hotmail.com>
2025-10-06 15:56:20 +1100

    added conform settings, package info

--- .config/nvim/lua/harry/plugins/conform.lua
+++ .config/nvim/lua/harry/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "isort", "black" },
+			astro = { "prettier", stop_after_first = true },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescript = { "prettierd", "prettier", stop_after_first = true },
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
@@ -11,6 +12,7 @@ return {
 			json = { "prettierd", "prettier", stop_after_first = true },
 			html = { "prettierd", "prettier", stop_after_first = true },
 			css = { "prettierd", "prettier", stop_after_first = true },
+			cpp = { "clang-format", stop_after_first = true },
 		},
 	},
 }
