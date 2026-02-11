18d913f00c95b75644614c90cd691b0e6bd9c7a3
Harry Ross <hazross@hotmail.com>
2026-02-11 20:16:07 +1100

    changed rofi style, removed wlogout

--- .config/rofi/powermenu/type-3/powermenu.sh
+++ .config/rofi/powermenu/type-3/powermenu.sh
@@ -11,7 +11,7 @@
 
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-3"
-theme='style-1'
+theme='style-5'
 
 # CMDs
 uptime="`uptime -p | sed -e 's/up //g'`"
@@ -94,6 +94,8 @@ case ${chosen} in
 			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
+    elif [[ -x '/usr/bin/hyprlock' ]]; then
+			hyprlock
 		fi
         ;;
     $suspend)
